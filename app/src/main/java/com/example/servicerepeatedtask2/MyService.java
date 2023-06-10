371f141122fb814b46b7c3d22c0d636857c9aa40
plutone <nisalvato@gmail.com>
2023-06-10 23:38:59 +0200

    ServiceBackGround

---
+++ app/src/main/java/com/example/servicerepeatedtask2/MyService.java
@@ -0,0 +1,78 @@
+package com.example.servicerepeatedtask2;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class MyService extends Service {
+    // constant
+    public static final long NOTIFY_INTERVAL = 10 * 1000; // 10 seconds
+
+    // run on another Thread to avoid crash
+    private Handler mHandler = new Handler(Looper.getMainLooper());
+    // timer handling
+    private Timer mTimer = null;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        // cancel if already existed
+        if (mTimer != null) {
+            mTimer.cancel();
+        } else {
+            // recreate new
+            mTimer = new Timer();
+        }
+        // schedule task
+        mTimer.scheduleAtFixedRate(new TimeDisplayTimerTask(), 0, NOTIFY_INTERVAL);
+    }
+
+    @Override
+    public void onDestroy() {
+
+        Toast.makeText(getApplicationContext(), "Service destroyed",
+                Toast.LENGTH_LONG).show();
+        mTimer.cancel();
+        super.onDestroy();
+    }
+
+    class TimeDisplayTimerTask extends TimerTask {
+
+        @Override
+        public void run() {
+            // run on another thread
+            mHandler.post(new Runnable() {
+
+                @Override
+                public void run() {
+                    // display toast
+                    Toast.makeText(getApplicationContext(), getDateTime(),
+                            Toast.LENGTH_LONG).show();
+                }
+
+
+            });
+        }
+
+
+        private String getDateTime() {
+            // get date time in custom format
+            SimpleDateFormat sdf = new SimpleDateFormat("[yyyy/MM/dd - HH:mm:ss]");
+            return sdf.format(new Date());
+        }
+
+    }
+}
